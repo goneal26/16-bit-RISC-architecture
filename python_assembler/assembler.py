e993c90f2f7d2a949dae51e84d6b0a0023f7b5e6
goneal26 <g.oneal26@ncf.edu>
2023-10-05 11:49:36 -0400

    Create assembler.py

---
+++ python_assembler/assembler.py
@@ -0,0 +1,4 @@
+# instructions
+OPS = ['halt', 'add', 'sub', 'mul', 'and', 'or', 'xor', 'not', 'shl', 'shr', 'copy', 'mvi', 'load', 'store', 'jump']
+MACROS = ['label'] # starting it as an array for now, in case I add more later
+
